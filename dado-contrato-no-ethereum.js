d1dc9fe947e16ec2f4c1e3035a95cbaede4b4c19
Jeff Prestes <jeffprestes@gmail.com>
2021-07-01 20:11:51 -0300

    Add files via upload

--- dado-contrato-no-ethereum.js
+++ dado-contrato-no-ethereum.js
@@ -1,6 +1,8 @@
 const contractAddress = "0x3Dd7335Ee100Dc8B4f51531Cc45228A18BFd7347";
 var smartContract;
 var smartContractWithSigner;
+//Both ways are valid
+//const ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"paramLocator","type":"string"},{"internalType":"string","name":"paramRenter","type":"string"},{"internalType":"string","name":"paramAddressHome","type":"string"},{"internalType":"uint256","name":"paramRentalValue","type":"uint256"}],"name":"registerRental","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"rentals","outputs":[{"internalType":"string","name":"locator","type":"string"},{"internalType":"string","name":"renter","type":"string"},{"internalType":"string","name":"addressHome","type":"string"},{"internalType":"uint256","name":"rentalValue","type":"uint256"}],"stateMutability":"view","type":"function"}];
 const ABI = [
 	{
 		"inputs": [
