96144d6e17bc9ce871a73ebc5559aa50a81ef4d7
Jeff Prestes <jeffprestes@gmail.com>
2021-07-01 11:50:18 -0300

    Salva dados no Smart Contract

---
+++ funcoes-contrato-ethereum.js
@@ -0,0 +1,56 @@
+async function getRentalContractData() {
+  let contractNumberField = document.getElementById("contractNumber");
+  const userInput = contractNumberField.value * 1;
+  alert("valorInformadoPeloUsuario: " + userInput);
+  try {
+    const arrayRentalData = await smartContract.rentals(userInput);
+    console.log(arrayRentalData);
+    //Modo mais verboso
+    let showLocator = document.getElementById("locator");
+    showLocator.innerText = arrayRentalData[0];
+    //Modo sintetico onde se concatena um comando junto a outro na mesma linha
+    document.getElementById("renter").innerText = arrayRentalData[1];
+    document.getElementById("addressHome").innerText = arrayRentalData[2];
+    document.getElementById("rentalValue").innerText = arrayRentalData[3];
+  } catch (err) {
+    console.error(err);
+    document.getElementById("locator").innerText = "";
+    document.getElementById("renter").innerText = "";
+    document.getElementById("addressHome").innerText = "";
+    document.getElementById("rentalValue").innerText = "";
+    contractNumberField.value = 0;
+    alert("Houve um erro ao buscar o contrato de numero: " + userInput);
+  }
+}
+
+async function autoLoadOwner() {
+  try {
+    const contractOwner = await smartContract.owner();
+    console.log(contractOwner);
+    document.getElementById("spanOwner").innerText = contractOwner;
+  } catch (err) {
+    console.error(err);
+    alert("Houve um erro ao buscar o proprietário do contrato");
+  }
+}
+
+async function saveFormData() {
+  try {
+    var tx;
+    var txReceipt;
+    tx = await smartContractWithSigner.registerRental(
+      document.frmImovel.paramLocator.value,
+      document.frmImovel.paramRenter.value,
+      document.frmImovel.paramAddressHome.value,
+      document.frmImovel.paramRentalValue.value
+    );
+    console.log("transacao enviada ao metamask. pendente...", tx);
+    alert("Transação enviada... aguarde a confirmação da Blockcnain...");
+    txReceipt = await tx.wait();
+    console.log("transacao processada...", txReceipt);
+    alert("Transação processada. Registro salvo na Blockchain");
+  } catch (err) {
+    console.error(err);
+    alert("Houve um erro ao salvar o registro do contrato de aluguel");
+  }
+}
